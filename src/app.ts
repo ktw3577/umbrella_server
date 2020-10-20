6de649ea0c52267de479ee448843f8317fa840ca
ktw3577 <taewongim06@gmail.com>
2020-10-20 10:25:54 +0900

    [server] /#28/ add:image upload feater
    이미지 업로드기능 추가.

--- src/app.ts
+++ src/app.ts
@@ -7,8 +7,14 @@ import morgan from 'morgan';
 import { sequelize } from './model';
 import router from './route';
 import passport from 'passport';
-
+import AWS from 'aws-sdk';
 const app = express();
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: 'ap-northeast-2',
+});
 
 app.use(
   session({
